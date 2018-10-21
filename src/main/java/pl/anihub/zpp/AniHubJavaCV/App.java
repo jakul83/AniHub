0d2288177003c47bf06ea6015be9f3d714e0e136
JK <jaroslaw.kulik@tlen.pl>
2018-10-21 12:09:55 +0200

    Project start.

---
+++ src/main/java/pl/anihub/zpp/AniHubJavaCV/App.java
@@ -0,0 +1,13 @@
+package pl.anihub.zpp.AniHubJavaCV;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
